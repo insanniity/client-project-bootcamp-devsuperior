54c5548e7a899fbce087ccef86f64e0f789ad6f9
insanniity <cleydson.cg@live.com>
2021-01-20 08:45:27 -0300

    Service find all paged

---
+++ src/main/java/com/insannity/client/services/ClientService.java
@@ -0,0 +1,25 @@
+package com.insannity.client.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.insannity.client.dto.ClientDTO;
+import com.insannity.client.entities.Client;
+import com.insannity.client.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));		
+	}
+	
+}
