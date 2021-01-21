cd9bac907ab5312d4ef9e5271ee2139db4c8ccb8
insanniity <cleydson.cg@live.com>
2021-01-20 21:03:03 -0300

    Client Update

--- src/main/java/com/insannity/client/resources/ClientResource.java
+++ src/main/java/com/insannity/client/resources/ClientResource.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,13 @@ public class ClientResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity <ClientDTO> findById(@PathVariable Long id){
 		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity <ClientDTO> update (@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
